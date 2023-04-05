b547c48bf51523ad2ae8e0cd041d866d891780f3
Luca Penasa <luca.penasa@gmail.com>
2023-04-05 16:00:24 +0200

    updated version and added __version__

--- juice-core-uplink-api-client/juice_core/__init__.py
+++ juice-core-uplink-api-client/juice_core/__init__.py
@@ -5,4 +5,9 @@ in a more convenient way than the generated code.
 May functionalities are not yet implemented, but some are.
 """
 
+from importlib_metadata import version
+
+__version__ = version("juice-core-uplink-api-client")
+
+
 from .SHTRestInterface import SHTRestInterface
