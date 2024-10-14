a26f632e4bb49af4c33b0e3d50209fff64cffc1f
Luca Penasa <luca.penasa@gmail.com>
2024-10-14 09:30:22 +0200

    small fix

--- src/juice_core_uplink_api_client/__init__.py
+++ src/juice_core_uplink_api_client/__init__.py
@@ -1,9 +1,5 @@
 """A client library for accessing Juice Core Uplink API"""
 
-import importlib
-
-__version__ = importlib.metadata.version("juice_core_uplink_api_client")
-
 from .client import AuthenticatedClient, Client
 
 __all__ = (
--- tests/test_can_import.py
+++ tests/test_can_import.py
@@ -4,5 +4,5 @@ def test_import_package():
 
 def test_has_version():
     """Check that the package has an accesible __version__"""
-    import juice_core_uplink_api_client
-    version = juice_core_uplink_api_client.__version__
+    import juice_core
+    version = juice_core.__version__
